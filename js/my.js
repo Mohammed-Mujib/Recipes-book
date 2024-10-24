75dac5581456758a30e3b9901ab8f845cf8d18a9
Mohammed-Mujib <wadmujib@gmail.com>
2024-10-25 00:16:39 +0300

    v.1

--- js/my.js
+++ js/my.js
@@ -1,72 +1,13 @@
-// let myobject = {
-//     title:"saitama",
-//     body: 'of steel',
-//     userId: 1,
-// }
-// async function getData(params) {
-//     let response = await fetch("https://jsonplaceholder.typicode.com/posts",{
-//         method:"POST",
-//         body: JSON.stringify(myobject),
-//         headers: {
-//             'Content-type': 'application/json; charset=UTF-8',
-//         },
-//     });
-//     let data = await response.json()
-//     console.log(data);
-    
-// }
-// getData()
-
-// let upobject = {
-//     userId : 1,
-//     title :"chapter 1",
-//     body :"the ultimit gatcha",
-//     id : 1,
-// }
-
-// async function updateData() {
-//     let response = await fetch(`https://jsonplaceholder.typicode.com/posts/${upobject.id}`,{
-//         method:"PUT",
-//         body: JSON.stringify(upobject),
-//         headers: {
-//             'Content-type': 'application/json; charset=UTF-8',
-//         },
-//     });
-//     let data = await response.json()
-//     console.log(data);
-    
-// }
-
-// updateData()
-// async function DeleteData() {
-//     let response = await fetch(`https://jsonplaceholder.typicode.com/posts/1`,{
-//         method:"DELETE",
-        
-//     });
-//     let data = await response.json()
-//     if (data.ok) {
-//         console.log("data has been deleted");   
-//     }
-// }
-// DeleteData()
-
-// let request = new XMLHttpRequest;
-
-var modal=document.getElementById("model");
-var single=document.getElementById("single");
-var link=document.querySelectorAll(".nav-link");
-var exampleModalLabel=document.getElementById("exampleModalLabel");
-var getMore=document.getElementById("getMore");
-var input1=document.getElementById("input1");
-var searchWords=document.getElementById("searchWords");
-var error=document.getElementById("error");
-var rowData=document.getElementById("data");
-// console.log(link);
-for (let i = 0; i < link.length ; i++) {
-    link[i].addEventListener('click',(e)=>{
-        getAllRecipes(e.target.innerHTML)
-    });
-}
+var modal = document.getElementById("model");
+var single = document.getElementById("single");
+var link = document.querySelectorAll(".nav-link");
+var modalLabel = document.getElementById("exampleModalLabel");
+var getMore = document.getElementById("getMore");
+var input1 = document.getElementById("input1");
+var searchWords = document.getElementById("searchWords");
+var error = document.getElementById("error");
+var rowData = document.getElementById("data");
+let recipes = []; 
 const items = [
     "carrot", "broccoli", "asparagus", "cauliflower", "corn", "cucumber",
     "green pepper", "lettuce", "mushrooms", "onion", "potato", "pumpkin", "red pepper",
@@ -91,33 +32,38 @@ const items = [
     "pork", "fish", "crab",
     "bacon", "ham", "pepperoni", "salami", "ribs"
 ];
-const lowercaseItems = items.map(item => item.toLowerCase());
 
-console.log(lowercaseItems);
-let recipes = []; 
-async function getAllRecipes(item = "pizza") {
-    let response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${item}`);
-    let data = await response.json()
-    recipes = data.recipes;
-    console.log(data.recipes);
-    displayRecipes()
-    input1.value = item;   
-    searchWords.innerHTML = "";
-    
+for (let i = 0; i < link.length ; i++) {
+    link[i].addEventListener('click',(e)=>{
+        getAllRecipes(e.target.innerHTML);
+    });
 }
 
+const lowercaseItems = items.map(item => item.toLowerCase());
+
+//  get all the recipes from the server and use the displayRecipes function ----------------------------------------- get recipes function
+async function getAllRecipes(item = "pizza") {
+    let response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${item}`);
+    let data = await response.json();
+    recipes = data.recipes;
+    // console.log(data.recipes);
+    displayRecipes();
+    input1.value = item;   
+    searchWords.innerHTML = "";
+}
+
+// activate when the user search for some thing and display the available options and mark the match ---------------- serach function
 function search(word) {
     let box = "";
     for (let i = 0; i < items.length; i++) {
         if (items[i].includes(word) && word !="") {
-            box += `<li class="p-2" onclick = "getAllRecipes('${items[i]}')" >${items[i].replace(word,`<span class = "span">${word}</span>`)} </li>` ;
+            box += `<li class="p-2" onclick = "getAllRecipes('${items[i]}')" >${items[i].replace(word,`<span class = "selection-color">${word}</span>`)} </li>` ;
         }
         if (box.length == 0) {
             error.classList.remove("d-none");
         } else {
             searchWords.innerHTML = box;
             error.classList.add("d-none");
-
         }
         if (input1.value == "") {
             error.classList.add("d-none");
@@ -127,23 +73,25 @@ function search(word) {
     searchWords.style.display= "block";
 }
 
+// activate the search function when the user search for some thing  ------------------------------------------------ event keyup
 input1.addEventListener("keyup",()=>{
     search(input1.value.toLowerCase())
-})
+});
 
 getAllRecipes('pizza');
 
+// display all the recipes founded in the server --------------------------------------------------------------------- recipes display function
 function displayRecipes() {
     let box = "";
     for (i of recipes) {
         box += `
         <div class="col-lg-4 col-md-6 col-sm-12 p-2 rounded">
-            <div class="all p-2 bg-white rounded mycard_re">
-                <div class="p-2 rounded img-holder">
+            <div class="all p-3 rounded recipe-card">
+                <div class=" rounded img-holder">
                     <img src="${i.image_url}" alt="" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick = 'getSingleRecipe(${i.recipe_id})'>
                 </div>
-                <div class="p-2">
-                    <h2>${i.title}</h2>
+                <div class=" pt-2">
+                    <h2 data-bs-toggle="modal" data-bs-target="#exampleModal" onclick = 'getSingleRecipe(${i.recipe_id})'>${i.title}</h2>
                     <p>${i.publisher}</p>
                 </div>
             </div>
@@ -155,21 +103,23 @@ function displayRecipes() {
 
 
 let singleRecipe = {};
+
 async function getSingleRecipe(id) {
     let response = await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${id}`)
     let data = await response.json()
     singleRecipe =data.recipe
     console.log(data.recipe);
-    displaySingleRecipe()
-    
+    displaySingleRecipe(singleRecipe)
 }
+
 getSingleRecipe()
-function displaySingleRecipe() {
+
+// get a single recipe informationfrom the ser
+function displaySingleRecipe(data) {
     let inger = "";
-    for ( info of singleRecipe.ingerdients) {
-        inger += `<li>${i}</li>`;
+    for ( info of data.ingredients) {
+        inger += `<li>${info}</li>`;
     }
-    let box = `
-    
-    `;
+    modalLabel.innerHTML = data.title
+    modal.innerHTML = `<ul>${inger}</ul>`
 }
